a7e82bdf37e7a009932b738355e99384868892ee
David Fridovich-Keil <dfk@berkeley.edu>
2020-05-07 11:02:28 -0400

    moving js stuff to html default

--- page.js
+++ page.js
@@ -8,21 +8,3 @@ document.getElementById("page_header").innerHTML =
     "<li><a href='/side_projects'>Side Projects</a></li>" +
     "<li><a href='pdfs/dfk_curriculum_vitae.pdf'>CV</a></li>"
 "</ul>";
-
-var slideIndex = 1;
-showDivs(slideIndex);
-
-function plusDivs(n) {
-    showDivs(slideIndex += n);
-}
-
-function showDivs(n) {
-    var i;
-    var x = document.getElementsByClassName("mySlides");
-    if (n > x.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = x.length} ;
-    for (i = 0; i < x.length; i++) {
-        x[i].style.display = "none";
-    }
-    x[slideIndex-1].style.display = "block";
-}
