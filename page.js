d0acc37655c6694830e5a8f7080a4930fc0e6393
David Fridovich-Keil <dfk@berkeley.edu>
2020-05-07 10:32:08 -0400

    fixes to header

--- page.js
+++ page.js
@@ -2,12 +2,14 @@ document.getElementById("page_footer").innerHTML =
 "<p>&copy;  " + new Date().getFullYear() + " David Fridovich-Keil. All rights reserved.</p>";
 
 document.getElementById("page_header").innerHTML =
+"<div class='org'>"
 "<ul id='menu'>" +
     "<li><a href='/'>Home</a></li>" +
     "<li><a href='/research'>Research</a></li>" +
     "<li><a href='/side_projects'>Side Projects</a></li>" +
     "<li><a href='pdfs/dfk_curriculum_vitae.pdf'>CV</a></li>"
-"</ul>";
+"</ul>" +
+"</div>";
 
 var slideIndex = 1;
 showDivs(slideIndex);
